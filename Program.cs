cb3994c2ba4bb5214529c29599672363230c0a7d
Ruslan Aliyev <ruslanra@code.edu.az>
2023-07-24 19:50:35 +0400

    I add names for case

--- Program.cs
+++ Program.cs
@@ -30,11 +30,29 @@ namespace MarketDemo
                     Console.WriteLine("Enter an option please:");
                     Console.WriteLine("----------------");
                 }
-            
-            
-            
-            
-            
+
+                switch (option)
+                {
+                    case 1:
+                     // MenuService.MenuAddNewProduct();
+                    case 2:
+                     // MenuService.MenuEditProduct();
+                    case 3:
+                     // MenuService.MenuDeleteProduct();
+                     case 4:
+                     // MenuService.MenuShowAllProducts():
+                     case 5:
+                     // MenuService.MenuShowProductsForCatagory();
+                     case 6:
+                     // MenuService.MenuShowProductsForPriceRange();
+                     case 7:
+                     // MenuService.MenuSearchProductbyName();
+                    default:
+                        break;
+                }
+
+
+
             } while (option == 0);
         }
     }
