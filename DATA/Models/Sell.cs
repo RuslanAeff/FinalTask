9b445fb826a9b378f28beb34fb437801ae97dd3f
Ruslan Aliyev <ruslanra@code.edu.az>
2023-07-24 19:25:52 +0400

    i created basics

---
+++ DATA/Models/Sell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarektDemo.Models
+{
+    internal class Sell: BaseEntity
+    {
+        public int Amount { get; set; }
+        public string Sellitem { get; set; }
+        public DateTime Time { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using MarektDemo.Services.Concrete;
+using System;
+using System.Data.SqlTypes;
+
+namespace MarketDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int option;
+
+            do
+            {
+                Console.WriteLine("1. Mehsullar uzerinde emeliyyat aparmaq");
+                Console.WriteLine("2. Satishlar uzerinde emeliyyat aparmaq");
+                Console.WriteLine("3. Sistemden chixish");
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Please enter option:");
+                Console.WriteLine("------------------------");
+
+
+
+
+                while (!int.TryParse(Console.ReadLine(), out option));
+            
+            
+            
+            
+            
+            } while (option == 0);
+        }
+    }
+}
